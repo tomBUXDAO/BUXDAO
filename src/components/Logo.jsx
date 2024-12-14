ea6c8e09ccc5bd10ce56516f295f2f28125d0de1
Tom-BUXDAO <Buxdao@gmail.com>
2024-12-14 20:38:13 +0000

    restore: add back logo image effects - Restore blur effect on logo image - Add back fade/opacity transition - Keep hover interactions - Maintain new positioning and sizing

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,14 +4,14 @@ const Logo = () => {
       <img 
         src="/logo.png" 
         alt="BUXDAO Logo" 
-        className="h-12 w-auto object-contain filter blur-[1px] opacity-85 transition-all duration-300 hover:opacity-100 hover:blur-0" 
+        className="h-10 w-auto object-contain filter blur-[1px] opacity-85 transition-all duration-300 hover:opacity-100 hover:blur-0" 
       />
       <span 
-        className="absolute left-1/2 -translate-x-1/2 whitespace-nowrap font-poppins text-3xl font-bold tracking-normal"
+        className="absolute left-1/2 -translate-x-1/2 whitespace-nowrap font-poppins text-lg sm:text-xl font-bold tracking-normal"
         style={{
           color: 'transparent',
-          WebkitTextStroke: '1.5px #FFD700',
-          textStroke: '1.5px #FFD700'
+          WebkitTextStroke: '1px #FFD700',
+          textStroke: '1px #FFD700'
         }}
       >
         BUX&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DAO
