da2199e98c82d7bb7924e5ce049d5883d42e829f
Tom-BUXDAO <Buxdao@gmail.com>
2024-12-15 17:03:05 +0000

    fix: remove text-size-adjust property - Remove all text-size-adjust properties - Keep font smoothing and rendering - Improve cross-browser compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,15 @@
 export default {
   plugins: {
     'postcss-preset-env': {
-      stage: 3,
+      stage: 0,
       features: {
         'nesting-rules': true,
-        'custom-properties': false,
-      },
+      }
     },
     tailwindcss: {},
     autoprefixer: {
-      grid: 'autoplace',
-      flexbox: true,
-      overrideBrowserslist: ['last 2 versions', '> 1%', 'not dead']
+      grid: true,
+      flexbox: true
     }
   }
 }
