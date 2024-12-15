ba20586510a4d46b493c19c3bc16c1c82609c9bc
Tom-BUXDAO <Buxdao@gmail.com>
2024-12-15 16:58:50 +0000

    fix: improve CSS compatibility - Update PostCSS configuration - Add proper browser targeting - Simplify base CSS - Use standard text rendering - Remove problematic vendor prefixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,17 @@
 export default {
   plugins: {
     'postcss-preset-env': {
+      stage: 3,
       features: {
         'nesting-rules': true,
+        'custom-properties': false,
       },
-      autoprefixer: {
-        flexbox: true,
-        grid: true
-      },
-      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'not dead']
     },
     tailwindcss: {},
-  },
+    autoprefixer: {
+      grid: 'autoplace',
+      flexbox: true,
+      overrideBrowserslist: ['last 2 versions', '> 1%', 'not dead']
+    }
+  }
 }
