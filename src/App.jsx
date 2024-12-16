ab90eae3aaf94b6ec4c5b13a4a50f1b9ab15dce2
Tom-BUXDAO <Buxdao@gmail.com>
2024-12-16 16:02:43 +0000

    style: adjust revenue sources container spacing - Reduce vertical spacing between items - Adjust margin below heading - Reduce padding between title and description - Update line height for descriptions - Match height with token metrics container

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Collection from './components/Collection';
+import BuxInfo from './components/BuxInfo';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Hero />
       <Collection />
+      <BuxInfo />
     </div>
   );
 }
