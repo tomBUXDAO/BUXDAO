20faa4d754d07c3fe8842be94b23c55ab6ef9a0d
Thomas Garner <tombuxdao@Thomass-iMac.Home>
2025-02-20 02:04:16 +0000

    fix: Remove all polyfills and simplify Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import postcssPresetEnv from 'postcss-preset-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,32 +18,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      stream: 'stream-browserify',
-      crypto: 'crypto-browserify',
-      path: 'path-browserify',
-      buffer: 'buffer',
-      util: 'util',
-      process: 'process/browser',
-      zlib: 'browserify-zlib',
-      http: null,
-      https: null,
-      os: null,
-      fs: null,
-      net: null,
-      tls: null,
-      child_process: null
-    }
-  },
-  define: {
-    'process.env': {},
-    global: 'globalThis'
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      }
+      '@': path.resolve(__dirname, './src')
     }
   }
 }) 
