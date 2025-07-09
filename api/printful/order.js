94ecceff5d08a2958844c84e1ed232573b4da3e6
Tom Garner <tombuxdao@Toms-iMac.Home>
2025-07-09 06:23:19 +0100

    fix: Add US state abbreviation mapping for Printful orders
    - Add getStateCode() helper function to map full state names to 2-letter codes
    - Update createPrintfulOrder() to use state abbreviations for US addresses
    - Fixes Printful validation error: 'state and ZIP code don't match'
    - Resolves issue where 'North Dakota' was sent instead of 'ND'

--- api/printful/order.js
+++ api/printful/order.js
@@ -203,6 +203,72 @@ function getCountryCode(countryName) {
   return countryMap[countryName] || countryName;
 }
 
+// Helper function to map US state names to 2-letter codes
+function getStateCode(stateName) {
+  const stateMap = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+    'District of Columbia': 'DC',
+    'American Samoa': 'AS',
+    'Guam': 'GU',
+    'Northern Mariana Islands': 'MP',
+    'Puerto Rico': 'PR',
+    'United States Minor Outlying Islands': 'UM',
+    'U.S. Virgin Islands': 'VI',
+    'Virgin Islands': 'VI'
+  };
+  
+  return stateMap[stateName] || stateName;
+}
+
 // Helper function to create Printful order
 async function createPrintfulOrder(shippingInfo, cart) {
   try {
@@ -217,7 +283,7 @@ async function createPrintfulOrder(shippingInfo, cart) {
       address1: shippingInfo.address1,
       address2: shippingInfo.address2 || null,
       city: shippingInfo.city,
-      state_code: shippingInfo.state,
+      state_code: mappedCountry === 'US' ? getStateCode(shippingInfo.state) : shippingInfo.state,
       country_code: mappedCountry,
       zip: shippingInfo.postalCode,
       phone: shippingInfo.phone || null,
